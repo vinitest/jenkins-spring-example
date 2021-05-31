5ea13eea638780f19cb0415cb4ca9e49be54a472
vinitest <hide-and-seek@wp.pl>
2021-05-31 21:49:26 +0200

    test

---
+++ src/main/java/com/example/jenkinsspringexample/JenkinsSpringExampleApplication.java
@@ -0,0 +1,20 @@
+package com.example.jenkinsspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsSpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringExampleApplication.class, args);
+    }
+
+    @GetMapping
+    public static String get() {
+        return "Hello Gosia";
+    }
+}
